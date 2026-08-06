d7c00086885334d746f1ca3c60288a73ad4df37e
omerayhan43 <omerayhan43@gmail.com>
2026-08-06 22:31:37 +0300

    app.py

--- app.py
+++ app.py
@@ -25,10 +25,10 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-# --- EXCEL DÖNÜŞTÜRME FONKSİYONU ---
+# --- EXCEL DÖNÜŞTÜRME FONKSİYONU (Hata Düzeltildi) ---
 def to_excel(df):
     output = BytesIO()
-    with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+    with pd.ExcelWriter(output) as writer:
         df.to_excel(writer, index=False, sheet_name='PortfoyAdaylari')
     return output.getvalue()
 
